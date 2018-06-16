525e741b580ff9a12b5db959bfb23a5ab2bf0e8e
Fbo28 <39802805+Fbo28@users.noreply.github.com>
2018-06-16 17:23:18 -0500

    Update Esfera.py

--- Esfera.py
+++ Esfera.py
@@ -1,8 +1,15 @@
+import math
+#Área Esfera
+def area_esfera(r):
+    area=(4*math.pi*(r**2))
+    return area
+
 #Volumen Esfera
 def volumen_esfera():
-    vesfera = ((4*3.14*(r**3))/3)
-    return vesfera
+   vesfera = ((4*math.pi*(r**3))/3)
+   return vesfera
 
 r = float(input("Radio: "))
 
-print("El volumen de la Esfera es:",round(volumen_esfera(),2))
+print("El volumen de la Esfera es:",round(volumen_esfera(r),2))
+print("El área es: ",round(area_esfera(r),2))
