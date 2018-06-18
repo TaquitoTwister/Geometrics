ff9e4abc33a9c9be7834e21cc9507084e6aef00b
TaquitoTwister <40031728+TaquitoTwister@users.noreply.github.com>
2018-06-17 19:18:44 -0500

    Update Cono.py
    Dar espacio luego de los dos puntos en print y generatriz no se pide, se calcula con los datos del radio y altura. (Cualquier opinión escribirla)

--- Cono.py
+++ Cono.py
@@ -1,28 +1,22 @@
-import math
+radcono =  float(input("Radio: "))
+hcono =   float(input("Altura: "))
+gene = math.sqrt(hcono**2 + radcono**2)
 
-r =  float(input("Radio: "))
-h =   float(input("Altura: "))
-g =  float(input("Generatriz: "))
-
-
+#Volumen Cono
 def volumen_cono():
-    vcono = ((math.pi * r**2)  * h) / 3
+    vcono = ((math.pi * radcono**2)  * hcono) / 3
     return  vcono
 
-
-
+#Área Total Cono
 def area_total_cono():
-    acono = (math.pi  * r  * g) + math.pi * r**2
+    acono = (math.pi  * radcono  * gene) + math.pi * radcono**2
     return acono
 
-
+#Área Lateral Cono
 def area_lateral_cono():
-    alcono = math.pi  * r  * g
+    alcono = math.pi  * radcono  * gene
     return alcono
 
-
-
-
-print("El área lateral del Cono es:",round(area_lateral_cono(),2))
-print("El área total del Cono es:",round(area_total_cono(),2))
-print("El volumen del Cono es:",round(volumen_cono(),2))
+print("El área lateral del Cono es: ",round(area_lateral_cono(),2))
+print("El área total del Cono es: ",round(area_total_cono(),2))
+print("El volumen del Cono es: ",round(volumen_cono(),2))
