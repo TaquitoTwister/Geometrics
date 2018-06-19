ce85329c6a6d95262b59fdad9dc205ae43b38040
TaquitoTwister <40031728+TaquitoTwister@users.noreply.github.com>
2018-06-19 10:29:30 -0500

    Update Esfera.py

--- Esfera.py
+++ Esfera.py
@@ -1,15 +1,17 @@
+#import math va en el código principal
+
 if figura == "esfera":
     #Área Esfera
     def area_esfera():
-        area=(4*math.pi*(r**2))
+        area=(4*math.pi*(resfera**2))
         return area
 
     #Volumen Esfera
     def volumen_esfera():
-       vesfera = ((4*math.pi*(r**3))/3)
+       vesfera = ((4*math.pi*(resfera**3))/3)
        return vesfera
 
-    r = float(input("Radio: "))
+    resfera = float(input("Radio: "))
 
     print("El área de la Esfera es: ",round(area_esfera(),2))
     print("El volumen de la Esfera es: ",round(volumen_esfera(),2))
